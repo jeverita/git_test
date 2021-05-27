c387bfea8388f1093c0b5263ed00ea1f381bab17
Jevera <yourname@example.comgit>
2021-05-28 03:05:33 +1000

    tests

--- Program.cs
+++ Program.cs
@@ -6,14 +6,9 @@ namespace git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            
-
+           
             Console.WriteLine("No!");
             Console.WriteLine("this is a new feature I wrote through command line");
-=======
-            Console.Writeline("No!");
-
         }
     }
 }
