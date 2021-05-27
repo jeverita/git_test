9e1a4c5f30ff9635be833261472d2218ee09c6a4
Jevera <yourname@example.comgit>
2021-05-28 02:55:33 +1000

    new stuff2

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ namespace git
         {
             Console.WriteLine("Hello World!");
             
-            Console.Writeline("No!");
-            System.Console.WriteLine("this is a new feature I wrote through command line");
+            Console.WriteLine("No!");
+            Console.WriteLine("this is a new feature I wrote through command line");
         }
     }
 }
